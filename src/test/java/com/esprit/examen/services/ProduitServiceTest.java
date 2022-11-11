6e407b60e3ae6226445953c5c0492eed319fe7f3
Iyadh Ben Ouarred <ibenouarred@gmail.com>
2022-11-11 02:18:47 +0100

    1st test

---
+++ src/test/java/com/esprit/examen/services/ProduitServiceTest.java
@@ -0,0 +1,71 @@
+package com.esprit.examen.services;
+
+import com.esprit.examen.entities.Produit;
+import com.esprit.examen.entities.Stock;
+import com.esprit.examen.repositories.ProduitRepository;
+import com.esprit.examen.repositories.StockRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.junit.runner.RunWith;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
+
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(MockitoExtension.class)
+public class ProduitServiceTest {
+    /* test git* */
+    @Autowired
+    IProduitService ps;
+
+    @Autowired
+    IStockService ss;
+
+    @MockBean
+    private ProduitRepository pr;
+
+    @MockBean
+    private StockRepository SR;
+
+
+    Produit p1=new Produit(1L,"p1","OMEN",4500,new Date(),new Date(),null,null,null);
+    Produit p2=new Produit(1L,"p2","MACBOOK",4000,new Date(),new Date(),null,null,null);
+    Stock S=new Stock(1L,"S1",100,3,null);
+
+    //Reruperer tout les produits
+    @Test
+    public void retrieveAllProduitsTest() {
+        when(pr.findAll()).thenReturn(Stream
+                .of(p1,p2)
+                .collect(Collectors.toList()));
+        assertEquals(2,ps.retrieveAllProduits().size());
+        System.out.println("Retrieved All Products With success");
+    }
+
+    
+}
