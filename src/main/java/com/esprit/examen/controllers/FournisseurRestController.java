7597ddfeeb9d444335bd4c8df6589d5abbb4e149
“Walid <walid.chekir@esprit.tn>
2022-11-15 09:49:21 -1100

    undo commit

--- src/main/java/com/esprit/examen/controllers/FournisseurRestController.java
+++ src/main/java/com/esprit/examen/controllers/FournisseurRestController.java
@@ -1,19 +1,6 @@
 package com.esprit.examen.controllers;
 
 import java.util.List;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,18 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.esprit.examen.entities.CategorieFournisseur;
-import com.esprit.examen.entities.DetailFournisseur;
-import com.esprit.examen.entities.Facture;
 import com.esprit.examen.entities.Fournisseur;
-import com.esprit.examen.entities.SecteurActivite;
 import com.esprit.examen.services.IFournisseurService;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.swagger.annotations.Api;
-import lombok.Getter;
-import lombok.Setter;
 
 
 @RestController
@@ -46,69 +25,47 @@ public class FournisseurRestController {
 	@Autowired
 	IFournisseurService fournisseurService;
 
+	// http://localhost:8089/SpringMVC/fournisseur/retrieve-all-fournisseurs
 	@GetMapping("/retrieve-all-fournisseurs")
 	@ResponseBody
 	public List<Fournisseur> getFournisseurs() {
-		return fournisseurService.retrieveAllFournisseurs();
+		List<Fournisseur> fournisseurs = fournisseurService.retrieveAllFournisseurs();
+		return fournisseurs;
 	}
 
+	// http://localhost:8089/SpringMVC/fournisseur/retrieve-fournisseur/8
 	@GetMapping("/retrieve-fournisseur/{fournisseur-id}")
 	@ResponseBody
 	public Fournisseur retrieveFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
 		return fournisseurService.retrieveFournisseur(fournisseurId);
 	}
 
+	// http://localhost:8089/SpringMVC/fournisseur/add-fournisseur
 	@PostMapping("/add-fournisseur")
 	@ResponseBody
-	public Fournisseur addFournisseur(@RequestBody FournisseurSonar f) {
-		return fournisseurService.addFournisseur(new Fournisseur(f.getIdFournisseur(), f.getCode(), f.getLibelle(), 
-				f.getCategorieFournisseur(), f.getFactures(), f.getSecteurActivites(), f.getDetailFournisseur()));
+	public Fournisseur addFournisseur(@RequestBody Fournisseur f) {
+		Fournisseur fournisseur = fournisseurService.addFournisseur(f);
+		return fournisseur;
 	}
 
+	// http://localhost:8089/SpringMVC/fournisseur/remove-fournisseur/{fournisseur-id}
 	@DeleteMapping("/remove-fournisseur/{fournisseur-id}")
 	@ResponseBody
 	public void removeFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
 		fournisseurService.deleteFournisseur(fournisseurId);
 	}
 
+	// http://localhost:8089/SpringMVC/fournisseur/modify-fournisseur
 	@PutMapping("/modify-fournisseur")
 	@ResponseBody
-	public Fournisseur modifyFournisseur(@RequestBody FournisseurSonar fournisseur) {
-		return fournisseurService.updateFournisseur(new Fournisseur(fournisseur.getIdFournisseur(), fournisseur.getCode(), fournisseur.getLibelle(), 
-				fournisseur.getCategorieFournisseur(), fournisseur.getFactures(), fournisseur.getSecteurActivites(), fournisseur.getDetailFournisseur())
-				);
+	public Fournisseur modifyFournisseur(@RequestBody Fournisseur fournisseur) {
+		return fournisseurService.updateFournisseur(fournisseur);
 	}
 
+	// http://localhost:8089/SpringMVC/fournisseur/assignSecteurActiviteToFournisseur/1/5
 		@PutMapping(value = "/assignSecteurActiviteToFournisseur/{idSecteurActivite}/{idFournisseur}")
 		public void assignProduitToStock(@PathVariable("idSecteurActivite") Long idSecteurActivite, @PathVariable("idFournisseur") Long idFournisseur) {
 			fournisseurService.assignSecteurActiviteToFournisseur(idSecteurActivite, idFournisseur);
 		}
 
 }
-
-
-@Getter
-@Setter
-class FournisseurSonar {
-
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idFournisseur;
-	private String code;
-	private String libelle;
-	@Enumerated(EnumType.STRING)
-	private CategorieFournisseur  categorieFournisseur;
-	@OneToMany(mappedBy="fournisseur")
-	@JsonIgnore
-	private Set<Facture> factures;
-    @ManyToMany
-    @JsonIgnore
-    private Set<SecteurActivite> secteurActivites;
-    @OneToOne(cascade= CascadeType.ALL,fetch=FetchType.EAGER)
-    private DetailFournisseur detailFournisseur;
-    
-
-	
-}
-
