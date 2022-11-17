a616791c4842ad13914f2b93bf766277a14291f4
samirbenslimene <73586428+samirbenslimene@users.noreply.github.com>
2022-11-17 01:15:11 +0100

    Update and rename CategorieProduitServiceImplMockitoTest.java to FactureServiceImplMockitoTest.java

---
+++ src/test/java/tn/esprit/rh/achat/FactureServiceImplMockitoTest.java
@@ -0,0 +1,41 @@
+package tn.esprit.rh.achat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tn.esprit.rh.achat.entities.Facture;
+import tn.esprit.rh.achat.repositories.FactureRepository;
+import tn.esprit.rh.achat.services.FactureServiceImpl;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class FactureServiceImplMockitoTest {
+
+    @Mock
+    FactureRepository cp;
+    @InjectMocks
+    FactureServiceImpl rs;
+    @Test
+    public void testAddCategorieProduit() {
+        //given
+        Facture fac = new Facture(10, 10 );
+
+        //when
+        Mockito.when(this.cp.save(Mockito.any())).thenReturn(fac);
+        Facture savedfac = rs.addFacture(fac);
+
+
+        //then
+        assertNotNull(savedfac.getIdFacture());
+
+
+    }
+
+}
